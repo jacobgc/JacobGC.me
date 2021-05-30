434b934e9a6587e4fe90a85d4eb538253664deb6
Jacob Gee-Clarke <jacob.geeclarke@uk.thalesgroup.com>
2021-05-30 22:17:48 +0100

    base commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"embed"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	"io/fs"
+	"net/http"
+)
+
+//go:embed assets
+var embeddedAssets embed.FS
+
+type embedFileSystem struct {
+	http.FileSystem
+}
+
+func (e embedFileSystem) Exists(prefix string, path string) bool {
+	_, err := e.Open(path)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func EmbedFolder(fsEmbed embed.FS, targetPath string) static.ServeFileSystem {
+	fsys, err := fs.Sub(fsEmbed, targetPath)
+	if err != nil {
+		panic(err)
+	}
+	return embedFileSystem{
+		FileSystem: http.FS(fsys),
+	}
+}
+
+func main() {
+	r := gin.Default()
+
+	r.Use(static.Serve("/", EmbedFolder(embeddedAssets, "assets")))
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
